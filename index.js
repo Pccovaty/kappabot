52fac6d4363c40e900a42b446b025128088a944b
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2018-09-02 22:38:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -284,5 +284,26 @@ bot.on("message", async message => {
       return;
       message.react("452183703267835910");
     }
+  if(cmd === `${prefix}prefixset`){
+     message.react("452183703267835910");
+  if (!message.member.hasPermission("MANAGE_SERVER")) return message.reply(":lock: Dostęp zablokowany. Nie posiadasz roli Moderator lub wyższej");
+  if (!args[0] || args[0 === "help"]) return message.reply("Użycie ``/prefixset <tutaj podaj nowy prefix>``");
+
+  const prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
+
+  prefixes[message.guild.id] = {
+    prefixes: args[0]
+  };
+
+  fs.writeFile("./prefixes.json", JSON.stringify(prefixes), (err) => {
+    if (err) console.log(err);
+  });
+
+  const sEmbed = new Discord.RichEmbed()
+    .setColor("#9b0090")
+    .setTitle("Prefix ustawiono pomyślnie!")
+    .setDescription(`prefix ustawiony! na ${args[0]}`);
+
+  message.channel.send(sEmbed);
 });
 bot.login(process.env.BOT_TOKEN);
